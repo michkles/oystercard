12d67a8a4dc14394fa2f295c1982a75f94eb04e0
crispinandrews <crispin.andrews@gmail.com>
2016-10-03 16:21:35 +0100

    challenge 8 complete. Journey, touch in and touch out added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,15 +1,27 @@
 require 'spec_helper'
 
-
 describe Oystercard do
 
   it 'has a balance of zero' do
     expect(subject.balance).to eq(0)
   end
 
-  describe '#top_up' do
-    it {is_expected.to respond_to(:top_up).with(1).argument}
+  it 'is not in a journey' do
+    expect(subject).not_to be_in_journey
+  end
 
+  it 'can touch in' do
+    subject.touch_in
+    expect(subject).to be_in_journey
+  end
+
+  it 'can touch out' do
+    subject.touch_in
+    subject.touch_out
+    expect(subject).not_to be_in_journey
+  end
+
+  describe '#top_up' do
     it 'can top up the balance' do
       expect{subject.top_up(1)}.to change{subject.balance}.by 1
     end
@@ -18,6 +30,13 @@ describe Oystercard do
       maximum_balance = Oystercard::MAXIMUM_BALANCE
       subject.top_up(maximum_balance)
       expect{subject.top_up 5}.to raise_error "Maximum balance of #{Oystercard::MAXIMUM_BALANCE} exceeded"
+    end
+  end
+
+  describe '#deduct' do
+    it 'deducts an amount from the balance' do
+      subject.top_up(10)
+      expect{subject.deduct 5}.to change{subject.balance}.by -5
     end
   end
 
