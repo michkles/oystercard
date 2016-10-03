0cf3849d8a2afb8ac52969cf94dab02e7d978dcd
Chetan Sanghani <crsanghani@gmail.com>
2016-10-03 17:08:35 +0100

    refactoring tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -21,14 +21,20 @@ describe Oystercard do
     expect(subject).not_to be_in_journey
   end
 
-  describe '#top_up' do
+  it "won't touch in if below minimum balance" do
+    expect{subject.touch_in}.to raise_error 'Insufficient funds for jouney'
+  end
+
+  describe 'top_up' do
+    before :each do
+      @balance = Oystercard::MAXIMUM_BALANCE
+    end
     it 'can top up the balance' do
       expect{subject.top_up(1)}.to change{subject.balance}.by 1
     end
 
     it 'raises an exception if maximum balance is exceeded' do
-      maximum_balance = Oystercard::MAXIMUM_BALANCE
-      subject.top_up(maximum_balance)
+      subject.top_up(@balance)
       expect{subject.top_up 5}.to raise_error "Maximum balance of #{Oystercard::MAXIMUM_BALANCE} exceeded"
     end
   end
