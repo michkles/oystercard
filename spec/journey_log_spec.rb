deb94a3b1634f4736bae3ad8e393b265d786b06a
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-10-05 16:05:28 +0200

    moved journey_history from journey to journey log

--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -2,4 +2,16 @@ require 'spec_helper'
 
 describe JourneyLog do
 
+
+  let(:card) {Oystercard.new(Oystercard::MINIMUM_FARE)}
+  let(:station1) {double(:station)}
+  let(:station2) {double(:station)}
+
+  describe '#initialize'
+    it "stores a whole journey in an array of hashes" do
+      card.touch_in(station1)
+      card.touch_out(station2)
+      expect(card.journey.journey_log.journey_history).to match_array([in: station1, out: station2])
+    end
+
 end
